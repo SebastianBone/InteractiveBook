0900551e050a7a716c5eed2628108d1fa31f8624
Sebastian Bohn <s.bohn@nodapo.de>
2016-05-01 20:17:23 +0200

    new Testscene and simple logik

---
+++ InteractiveBook/Assets/Scripts/FoodController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodController : MonoBehaviour {
+
+	public int amountOfFood;
+	public int maxAmountOfFood = 100;
+	bool buttonPressed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (buttonPressed) {
+			FillFood ();
+		}
+	}
+
+	public void FillFood(){
+
+		if (amountOfFood <= maxAmountOfFood) {
+			
+			amountOfFood += 1;
+		} 
+		if (amountOfFood == maxAmountOfFood) {
+			buttonPressed = false;
+		}
+	}
+
+	public void FillButtonPressed(){
+		buttonPressed = true;
+	}
+
+	public int getAmountOfFood(){
+		return amountOfFood;
+	}
+
+	public void DecreaseFood(){
+
+		amountOfFood -= 1;
+	}
+}
