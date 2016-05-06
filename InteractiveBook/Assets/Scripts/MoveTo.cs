01b169868b15aed5fa495e9b7c3b5833925f8c75
Marko Vukadinovic <m.vukadinovic@nodapo.de>
2016-05-06 14:59:54 +0200

    Added simple animation to character prefab
    Ref: https://github.com/SebastianBone/InteractiveBook

--- InteractiveBook/Assets/Scripts/MoveTo.cs
+++ InteractiveBook/Assets/Scripts/MoveTo.cs
@@ -1,27 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq.Expressions;
 
 public class MoveTo : MonoBehaviour {
 
-	private NavMeshAgent agent;
+	// Public properties
+
 	public GameObject fruit; 
 	public GameObject otherAnimal;
 	public GameObject animalChild;
 	public int amountOfFood = 0;
 	public int maxAmountOfFood = 10;
 
+	// Animation name getter.
+	public AnimationClip idle;
+	public AnimationClip run;
+	public AnimationClip cry;
+	public AnimationClip jump;
+	public AnimationClip fall;
+
+	// Private properties
+	private NavMeshAgent agent;
 	private GameObject house;
 	private GameObject feedingArea;
 	private GameObject[] explorePositions;
 	private int explorePositionsSize;
 	private int randomPosition;
 	private float waitTime;
-	private float feedTime = 2f;
+	private float feedTime = 1f;
 	private bool isExplorePosition = false;
+	private bool IsEating = false;
 
+	//Animation
+	private Animator anim;
 
 
 	void Start () {
+		anim = gameObject.GetComponent<Animator> ();
 		agent = GetComponent<NavMeshAgent>();
 		explorePositions = GameObject.FindGameObjectsWithTag ("explore");
 		if (explorePositions != null) {
@@ -33,13 +48,24 @@ public class MoveTo : MonoBehaviour {
 	}
 
 	void Update() {
+
+		if (!IsEating && !isExplorePosition) {
+			anim.Play (run.name);
+		} else {
+			anim.Play (idle.name);
+		}
+
 		fruit = GameObject.FindGameObjectWithTag ("food");
 		if (fruit != null) {
 			agent.SetDestination (fruit.transform.position);
+			// Run to the food container
+			if (!IsEating) {
+				isExplorePosition = false;
+				anim.Play (run.name);
+			}
 		}
 
 		if (amountOfFood >= maxAmountOfFood && otherAnimal != null) {
-
 			agent.SetDestination (otherAnimal.transform.position);
 		}
 
@@ -84,13 +110,16 @@ public class MoveTo : MonoBehaviour {
 	void OnTriggerStay(Collider collider){
 		
 		if (collider == feedingArea.GetComponent<BoxCollider> ()) {
-			Destroy (fruit, 2f);
+			Destroy (fruit, 1.5f);
+			IsEating = true;
 			feedTime -= Time.deltaTime;
 
 			if (feedTime <= 0) {
 				amountOfFood++;
 				feedTime = 2f;
 			}
+		} else {
+			IsEating = false;
 		}
 	}
 }
